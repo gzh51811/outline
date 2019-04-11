482f43cba4ffc70ebb22558ce25280f4a4c5f65d
aaron <jeroo@qq.com>
2019-04-11 16:02:47 +0800

    day8-4 微信小程序-项目（首页）

--- Wechat/myMusic/pages/index/index.js
+++ Wechat/myMusic/pages/index/index.js
@@ -1,28 +1,29 @@
 //index.js
 //获取应用实例
-const app = getApp();console.log(app)
+const app = getApp();
+console.log(app)
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    username:app.globalData.username,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    list_hot: [],
+    list_new: [],
+    recommends: [],
+    keyword: '',
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
   },
-  onLoad: function () {
+  onLoad: function() {
+    //通过微信接口获取用户信息
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -35,6 +36,7 @@ Page({
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
+          // console.log('用户信息：', res)
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
@@ -43,18 +45,50 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+    //获取百度热门音乐
+    this.getData({
+      url: 'http://tingapi.ting.baidu.com/v1/restserver/ting',
+      data: {
+        method: 'baidu.ting.billboard.billList',
+        type: 2,
+        size: 10,
+        offset: 0
+      }
+    }).then(({song_list})=>{
+      let recommends = song_list.sort((a,b)=>{
+        return b.hot - a.hot
+      }).slice(0,5);
+      
+      this.setData({
+        recommends,
+        keyword: recommends[0].title
+      });
     })
-  }, onPullDownRefresh() {
-    console.log('下拉刷新')
   },
-  onReachBottom() {
-    console.log('加载更多')
+  //ajax请求封装
+  getData({
+    url,
+    data
+  }) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url,
+        data,
+        success: res => {
+          resolve(res.data);
+        },
+        fail(err) {
+          reject(err)
+        }
+      });
+    })
+
   },
+
+  gotoSearch() {
+    wx.navigateTo({
+      url: '/pages/search/search?keyword=' + this.data.keyword,
+    })
+  }
 })
